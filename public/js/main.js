06d9012fcae0565ef376855a7991fef5cec72345
razdvaz <chronovos@yandex.ru>
2017-03-06 00:38:40 +0300

    add lists

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,14 @@
 $.ajax({
 	url: "data.json"
 }).done(function(data){
-	let template = $("#template").html();
+
+	template_it("#template_key_agg", data, '#key_agg')
+	template_it("#template_options", data, '#options')
+})
+
+function template_it(script_id, data, div_id){
+	let template = $(script_id).html();
 	let compile = Handlebars.compile(template);
 	let result = compile(data);
-	let content = $('#options').html(result);
-})
+	let contentg = $(div_id).html(result);
+}
